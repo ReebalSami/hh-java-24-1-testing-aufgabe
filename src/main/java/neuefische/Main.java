3f1bfea4b3e86d35274a1ed633dc332fb74fc4b4
Reebal Sami <reebal.sami@gmail.com>
2024-01-24 15:08:21 +0100

    inssial

--- src/main/java/neuefische/Main.java
+++ src/main/java/neuefische/Main.java
@@ -1,2 +1,21 @@
-package neuefische;public class Main {
+package neuefische;
+
+public class Main
+{
+    public static void main(String[] args){
+        int num1 = 1;
+        int num2 = 2;
+        int sum = addNum(num1, num2);
+        int product = productNum(num1, num2);
+        System.out.println(sum);
+        System.out.println(product);
+
+    }
+    public static int addNum(int a, int b) {
+        return a + b;
+    }
+
+    public static int productNum(int a, int b) {
+        return a * b;
+    }
 }
--- src/test/java/neuefische/MainTest.java
+++ src/test/java/neuefische/MainTest.java
@@ -1,4 +1,26 @@
+package neuefische;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+
+    @Test
+    void addNum() {
+        //given
+        int a = 1;
+        int b = 1;
+        int expected = 2;
+
+        //expected
+        int actual = Main.addNum(a, b);
+
+
+        //then
+        assertEquals(expected, actual);
+    }
+
+
+
 }
